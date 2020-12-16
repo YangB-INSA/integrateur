f475b8d66946322d406824b270b6ed2c51a09beb
Boris-INSA <borisyang1997@gmail.com>
2020-12-16 17:16:38 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,100 @@
+import dash
+
+import pandas as pd
+import plotly.express as px
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Iris bar figure
+def drawFigure():
+    return  html.Div([
+        dbc.Card(
+            dbc.CardBody([
+                dcc.Graph(
+                    figure=px.bar(
+                        df, x="sepal_width", y="sepal_length", color="species"
+                    ).update_layout(
+                        template='plotly_dark',
+                        plot_bgcolor= 'rgba(0, 0, 0, 0)',
+                        paper_bgcolor= 'rgba(0, 0, 0, 0)',
+                    ),
+                    config={
+                        'displayModeBar': False
+                    }
+                ) 
+            ])
+        ),  
+    ])
+
+# Text field
+def drawText():
+    return html.Div([
+        dbc.Card(
+            dbc.CardBody([
+                html.Div([
+                    html.H2("Text"),
+                ], style={'textAlign': 'center'}) 
+            ])
+        ),
+    ])
+
+# Data
+df = px.data.iris()
+
+# Build App
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = html.Div([
+    dbc.Card(
+        dbc.CardBody([
+            dbc.Row([
+                dbc.Col([
+                    html.Div([
+                        html.H1("French air traffic visualization"),
+                        html.H6("Made by Axolotl"),
+                    ], style={'textAlign': 'center'}),
+                ], width=3),
+                dbc.Col([
+                    html.Div([
+                        html.H1("French air traffic visualization"),
+                        html.H6("Made by Axolotl"),
+                    ], style={'textAlign': 'center'}),
+                ], width=6),
+                dbc.Col([
+                    html.Div([
+                        html.H1("French air traffic visualization"),
+                        html.H6("Made by Axolotl"),
+                    ], style={'textAlign': 'center'}),
+                ], width=3),
+            ], align='center'), 
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    drawFigure() 
+                ], width=3),
+                dbc.Col([
+                    drawFigure()
+                ], width=3),
+                dbc.Col([
+                    drawFigure() 
+                ], width=6),
+            ], align='center'), 
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    drawFigure()
+                ], width=9),
+                dbc.Col([
+                    drawFigure()
+                ], width=3),
+            ], align='center'),      
+        ]), color = 'dark'
+    )
+])
+
+# Run app and display result inline in the notebook
+if __name__ == "main":
+    app.run_server(debug=True,port="50000")
